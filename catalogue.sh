ef274dfc90d183e88779658342e64988f9cdc758
DELL <lakshmi.allu04@gmail.om>
2023-06-21 16:19:28 +0530

    value $a

--- catalogue.sh
+++ catalogue.sh
@@ -2,10 +2,5 @@ source common.sh
 component="catalogue"
 
 nodejs
+mongo_load_schema
 
-echo -e "${color}copy mongodb repo file${nocolor}"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-echo -e "${color}install mongodb client${nocolor}"
-yum install mongodb-org-shell -y &>>${log_path}
-echo -e "${color}load schema${nocolor}"
-mongo --host mongodb-dev.devops73.in </app/schema/catalogue.js &>>${log_path}
--- common.sh
+++ common.sh
@@ -29,3 +29,12 @@ nodejs(){
  systemctl enable $component &>>${log_path}
  systemctl restart $component &>>${log_path}
 }
+mongo_load_schema()
+{
+  echo -e "${color}copy mongodb repo file${nocolor}"
+  cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+  echo -e "${color}install mongodb client${nocolor}"
+  yum install mongodb-org-shell -y &>>${log_path}
+  echo -e "${color}load schema${nocolor}"
+  mongo --host mongodb-dev.devops73.in </app/schema/catalogue.js &>>${log_path}
+}

8ad7699f91997d974063304320ba54aed494c5b9
DELL <lakshmi.allu04@gmail.om>
2023-06-21 16:08:43 +0530

    value $a

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,5 @@
+source common.sh
 component="catalogue"
-color="\e[33m"
-nocolor="\e[0m"
-log_path="/tmp/roboshop.log"
-app_path="/app"
 echo -e "${color}configuration nodejs repos ${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_path}
 echo -e "${color}install nodejs\e[0m"
@@ -10,14 +7,14 @@ yum install nodejs -y &>>${log_path}
 echo -e "${color} add user application${nocolor}"
 useradd roboshop &>>${log_path}
 echo -e "${color} create application directory${nocolor}"
-rm -rf /app &>>${log_path}
-mkdir /app
+rm -rf ${app_path} &>>${log_path}
+mkdir ${app_path}
 echo -e "${color} download the applicaion conent${nocolor}"
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_path}
-cd /app
+cd ${app_path}
 echo -e "${color}extract the content${nocolor}"
 unzip /tmp/$component.zip &>>${log_path}
-cd /app
+cd ${app_path}
 echo -e "${color} install nodejs dependencies ${nocolor}"
 npm install &>>${log_path}
 echo -e "${color} setup systemd service ${nocolor}"

62a8455c103e948f83c3c1e789d561ce95d0ac1e
DELL <lakshmi.allu04@gmail.om>
2023-06-26 19:42:59 +0530

    drycode

--- payment.sh
+++ payment.sh
@@ -5,7 +5,7 @@ component=payment
 roboshop_add_password=$1
 
 if [ -z "$mysql_root_password" ]; then
-  echo "mysql_root_password is missing"
+  echo mysql_root_password is missing
   exit 1
   fi
 

ab1fc1d9a683a8d5fa73a2bdf4a8e026fcdd543e
DELL <lakshmi.allu04@gmail.om>
2023-06-26 15:22:01 +0530

    drycode

--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,17 @@
 source common.sh
 echo -e "${color} disable mysql default version${nocolor}"
 yum module disable mysql -y &>>/tmp/roboshop.log
-stat_check $
+stat_check $?
 echo -e "${color} copy mysql repo file${nocolor}"
 cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
-stat_check $
+stat_check $?
 echo -e "${color}\ install mysql community server ${nocolor}"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
-stat_check $
+stat_check $?
 echo -e "${color} start mysql service${nocolor}"
 systemctl enable mysqld &>>/tmp/roboshop.log
 systemctl restart mysqld &>>/tmp/roboshop.log
-stat_check $
+stat_check $?
 echo -e "${color} setup mysql password ${nocolor}"
 mysql_secure_installation --set-root-pass @1 &>>/tmp/roboshop.log
-stat_check $
+stat_check $?

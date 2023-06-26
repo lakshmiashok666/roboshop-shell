763feebba9dcdece5fa4f3110217bebc9a71851d
DELL <lakshmi.allu04@gmail.om>
2023-06-26 17:11:43 +0530

    drycode

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -14,6 +14,6 @@ systemctl enable rabbitmq-server &>>/tmp/roboshop.log
 systemctl start rabbitmq-server &>>/tmp/roboshop.log
 stat_check $?
 echo -e "${color} add rabbitmq application user${nocolor}"
-rabbitmqctl add_user roboshop @1 &>>/tmp/roboshop.log
+rabbitmqctl add_user roboshop $1 &>>/tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
 stat_check $?
